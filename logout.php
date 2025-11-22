edfe8fb4f581d16b22170a5c58bb48f5c9ab82d5
202100628 <202100628@stu.uob.edu.bh>
2025-11-22 22:24:37 +0300

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/inc/config.php';
-require_once __DIR__ . '/inc/flash.php';
-require_once __DIR__ . '/inc/auth.php';
-
-do_logout();
-flash_set('info', 'You have been logged out.');
-header('Location: /');
-exit;
